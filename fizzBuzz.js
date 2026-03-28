00a710687fc48a924158b85533aec70787fafb24
Muumz <kaisa.otterklau@gmail.com>
2026-03-28 20:47:05 +0200

    while loop

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,17 +2,23 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
-  var fizz = count % 3;
-  var buzz = count % 5;
-  if (fizz === 0 && buzz === 0) {
-    output.push("FizzBuzz");
-  } else if (fizz === 0) {
-    output.push("Fizz");
-  } else if (buzz === 0) {
-    output.push("Buzz");
-  } else {
-    output.push(count);
+  while (count <= 100) {
+    var fizz = count % 3;
+    var buzz = count % 5;
+    if (fizz === 0 && buzz === 0) {
+      output.push("FizzBuzz");
+    } else if (fizz === 0) {
+      output.push("Fizz");
+    } else if (buzz === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
   }
-  count++;
   console.log(output);
 }
+
+fizzBuzz();
+
+//added a while loop so theres no need to spam function fizzbuzz
